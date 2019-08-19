e5bde81c48a519fd4211a25262e99f6a5397cf4b
felipenrocha <felipenrocha19@gmail.com>
2019-08-19 16:11:47 -0300

    Aula 30 - 10:03

--- classes/teste.cpp
+++ classes/teste.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
@@ -8,13 +9,44 @@ public:
     char nome[25];
     char cpf[20];
     int idade;
-    
 };
-
-
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+
+    Pessoa p1; // Instancia de uma classe = objeto de Pessoa
+
+    strcpy(p1.nome, "João");
+    strcpy(p1.cpf, "02533523127");
+    p1.idade = 20;
+    cout << "Nome: " << p1.nome << endl;
+    cout << "CPF: " << p1.cpf << endl;
+    cout << "Idade: " << p1.idade << endl
+         << endl;
+
+    // Inicializando uma Classe:
+
+    Pessoa p2 = {"Felipe", "12345678", 18};
+
+    cout << "Nome: " << p2.nome << endl;
+    cout << "CPF: " << p2.cpf << endl;
+    cout << "Idade: " << p2.idade << endl
+         << endl;
+
+    // Inicializando varias classes
+
+    Pessoa pessoas[3] = {
+        {"Felipe", "12345678", 18},
+        {"Pedro", "2220154", 22},
+        {"Tiago", "8752202", 23}
+        };
+
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "Nome: " << pessoas[i].nome << endl;
+        cout << "CPF: " << pessoas[i].cpf << endl;
+        cout << "Idade: " << pessoas[i].idade << endl;
+    }
+
     return 0;
 }
